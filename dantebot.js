c8655a06b16e55ec64cbbf380c7e89ce2cf7c2d3
Daniel Turcotte <dturcotte@fuzzproductions.com>
2015-07-29 16:11:31 -0400

    dev: test

--- dantebot.js
+++ dantebot.js
@@ -7,73 +7,74 @@ module.exports = function (req, res, next) {
 	//botPayload.channel = req.body.channel_id;
 	botPayload.channel = '#crub';
 	botPayload.icon_url = 'http://i.imgur.com/IciaiJt.png';
-	botPayload.userToGet = req.body.text;
+
 	//botPayload.userToGet = 'aj';
+
+	//console.log('REQ BODY TEXT', req.body.text);
+
+	//curl -X GET --data "text=username" http://localhost:3002/sdc : mimics the user's input text in SLACK
+	//return res.status(200).json('HELLO ' + req.body.text);
 
 	/*
 		Get all user IDs first...
 	 */
-	getUserData(botPayload, function (error, status, body) {
-		//console.log('BODY',body);
-		if (error) {
-			return next(error);
-		} else if (status !== 200) {
-			return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-		} else {
 
-		//	console.log('getUserData', JSON.parse(body).user);
-			var val = JSON.parse(body);
+	if (req.body.text) {
+		botPayload.userToGet = req.body.text;
 
-			for (var i = 0; i < val.members.length; i++) {
-				if (val.members[i].name === botPayload.userToGet) {
-					botPayload.userID = val.members[i].id;
+		getUserData(botPayload, function (error, status, body) {
+			//console.log('BODY',body);
+			if (error) {
+				return next(error);
+			} else if (status !== 200) {
+				return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+			} else {
+
+				//	console.log('getUserData', JSON.parse(body).user);
+				var val = JSON.parse(body);
+
+				for (var i = 0; i < val.members.length; i++) {
+					if (val.members[i].name === botPayload.userToGet) {
+						botPayload.userID = val.members[i].id;
+					}
 				}
+				console.log('GETTING USER DATA', botPayload.userID + ', ....' + botPayload.userToGet);
+
+				getUserByID(botPayload, function (error, status, body) {
+					if (error) {
+						return next(error);
+					} else if (status !== 200) {
+						return next(new Error('Incoming WebHook: ' + status + ' ' + body));
+					} else {
+						//console.log('JSON.parse(body)', JSON.parse(body));
+						var name = JSON.parse(body).user.name;
+						var no_quotes = name.slice(0, name.length);
+						var image = JSON.parse(body).user.profile.image_48;
+
+						botPayload.username = no_quotes;
+						botPayload.icon_url = image;
+						botPayload.text = req.body.text;
+						//botPayload.text = 'HI THERE';
+
+						//return res.status(200).json(botPayload);
+						postToSlack(botPayload, function (error, status, body) {
+
+						});
+
+						//console.log('BODY', no_quotes);
+
+						//botPayload.text = body.real_name;
+
+					}
+
+				});
 			}
-			console.log('GETTING USER DATA', botPayload.userID + ', ....' + botPayload.userToGet);
-
-
-			getUserByID(botPayload, function (error, status, body) {
-				if (error) {
-					return next(error);
-				} else if (status !== 200) {
-					return next(new Error('Incoming WebHook: ' + status + ' ' + body));
-				} else {
-					//console.log('JSON.parse(body)', JSON.parse(body));
-					var name = JSON.parse(body).user.name;
-					var no_quotes = name.slice(0, name.length);
-					var image = JSON.parse(body).user.profile.image_48;
-
-
-					botPayload.username = no_quotes;
-					botPayload.icon_url = image;
-					botPayload.text = req.body.text;
-					//botPayload.text = 'HI THERE';
-
-					//return res.status(200).json(botPayload);
-					postToSlack(botPayload, function (error, status, body) {
-
-
-					});
-
-
-					//console.log('BODY', no_quotes);
-
-
-
-					//botPayload.text = body.real_name;
-
-				}
-
-			});
-		}
-	});
+		});
+	}
 };
 
 function getUserByID (payload, callback) {
 
-	if (payload.userID === null) {
-		payload.userID = 'U07D3PBUH';
-	}
 	console.log('adsjkfhadskjfhsadkjfhsadkhfkjsdf', payload.userID);
 
 
@@ -85,6 +86,7 @@ function getUserByID (payload, callback) {
 		if (error) {
 			return callback(error);
 		}
+		console.log('request received', body);
 		callback(null, response.statusCode, body);
 	});
 }
