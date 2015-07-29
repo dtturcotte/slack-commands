18de963c532a08530c8068438001f04f619d4168
Daniel Turcotte <dturcotte@fuzzproductions.com>
2015-07-29 14:11:25 -0400

    dev: get user id

--- dantebot.js
+++ dantebot.js
@@ -1,15 +1,16 @@
 var request = require('request');
 
 module.exports = function (req, res, next) {
-
-	console.log(req.body);
-
 	var botPayload = {};
-	botPayload.text = 'I wanna die!!!';
+	var user_to_get = req.body.text; //get user ID based on username
+
+	botPayload.text = '';
 	botPayload.username = 'my_new_bot';
 	//botPayload.channel = req.body.channel_id;
 	botPayload.channel = '#crub';
 	botPayload.icon_url = 'http://i.imgur.com/IciaiJt.png';
+	botPayload.userToGet = req.body.text;
+
 
 	/*
 		Get all user IDs first...
@@ -22,33 +23,45 @@ module.exports = function (req, res, next) {
 			return next(new Error('Incoming WebHook: ' + status + ' ' + body));
 		} else {
 
+		//	console.log('getUserData', JSON.parse(body).user);
+			var val = JSON.parse(body);
+
+			for (var i = 0; i < val.members.length; i++) {
+				if (val.members[i].name === botPayload.userToGet) {
+					botPayload.userID = val.members[i].id;
+				}
+			}
+
+
 			getUserByID(botPayload, function (error, status, body) {
 				if (error) {
 					return next(error);
 				} else if (status !== 200) {
 					return next(new Error('Incoming WebHook: ' + status + ' ' + body));
 				} else {
-					console.log('JSON.parse(body)', JSON.parse(body));
+					//console.log('JSON.parse(body)', JSON.parse(body));
 					var name = JSON.parse(body).user.name;
 					var no_quotes = name.slice(0, name.length);
 					var image = JSON.parse(body).user.profile.image_48;
 
+
 					botPayload.username = no_quotes;
 					botPayload.icon_url = image;
 					botPayload.text = req.body.text;
-					
+
+					//return res.status(200).json(botPayload);
 					postToSlack(botPayload, function (error, status, body) {
 
 
 					});
 
 
-					console.log('BODY', no_quotes);
+					//console.log('BODY', no_quotes);
 
 
 
 					//botPayload.text = body.real_name;
-					//return res.status(200).json(botPayload);
+
 				}
 
 			});
@@ -58,7 +71,7 @@ module.exports = function (req, res, next) {
 
 function postToSlack (payload, callback) {
 	var val = JSON.stringify(payload);
-	console.log('PAYLOAD', val);
+	//console.log('PAYLOAD', val);
 	request({
 		headers: {
 			'content-type': 'application/json'
@@ -70,7 +83,7 @@ function postToSlack (payload, callback) {
 		if (error) {
 			return callback(error);
 		}
-		console.log('RESPONSE', body);
+		//console.log('RESPONSE', body);
 		callback(null, response.statusCode, body);
 	});
 }
@@ -86,14 +99,18 @@ function getUserData (payload, callback) {
 		if (error) {
 			return callback(error);
 		}
+		//console.log('get user data', body);
 		callback(null, response.statusCode, body);
 	});
 }
 
 function getUserByID (payload, callback) {
 
+	if (payload.userID === null) {
+		payload.userID = 'U07D3PBUH';
+	}
 	request({
-		uri: 'https://slack.com/api/users.info?token=xoxp-2697721554-7445793969-8170611990-31095b&user=U07870WEB&pretty=1',
+		uri: 'https://slack.com/api/users.info?token=xoxp-2697721554-7445793969-8170611990-31095b&user=' + payload.userID + '&pretty=1',
 		method: 'GET',
 		body: JSON.stringify(payload)
 	}, function (error, response, body) {
